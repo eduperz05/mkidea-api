aec5e5defeee7b081c2fcf3defbd09d420bd81fc
Judith <judyfdez97@gmail.com>
2022-11-22 12:09:54 +0100

    Team Controller Added
    > getTeams, getTeam, postTeam and deleteTeam implemented

--- src/API/controllers/team/index.ts
+++ src/API/controllers/team/index.ts
@@ -1,20 +1,31 @@
-
 import { Request, Response } from "express";
+import { Team } from "../../models/team";
 
-export const getTeams = (req: Request, res: Response) => {
-  res.status(200).send("Users");
+export const getTeams = async(req: Request, res: Response) => {
+  const teams = await Team.findAll();
+  res.status(200).json({ teams });
+  return;  
 };
 
-export const getTeam = (req: Request, res: Response) => {
-  res.status(200).send("Users");
+export const getTeam = async(req: Request, res: Response) => {
+  const { id } = req.params;
+  const team = await Team.findByPk(id);
+  res.status(200).json({ team });
+  return;
 };
 
-export const postTeam = (req: Request, res: Response) => {
-  res.status(200).send("Users");
+export const postTeam = async(req: Request, res: Response) => {
+  let team = await Team.create({ ...req.body });
+  res.status(201).json({ team });
+  return;
 };
 
-export const deleteTeam = (req: Request, res: Response) => {
-  res.status(200).send("Users");
+export const deleteTeam = async(req: Request, res: Response) => {
+  const { id } = req.params;
+  const deleteTeam = await Team.findByPk(id);
+  Team.destroy({ where: { id } });
+  res.status(200).json({ deleteTeam });
+  return;
 };
 
 export const changeTeam = (req: Request, res: Response) => {
