f032f8f1173fef505f76d0ae88e7eb626c062d6f
“Romer” <romeralvareziv@gmail.com>
2022-12-14 12:56:03 +0000

    New parameters on user table

--- src/config/filterFields.ts
+++ src/config/filterFields.ts
@@ -1,4 +1,4 @@
-export const filterFieldsUser = ["id_user", "password", "createdAt", "updatedAt"];
+export const filterFieldsUser = ["id_user", "password", "createdAt", "updatedAt", "phone"];
 export const filterFieldsNews = ["id_news", "createdAt", "updatedAt"];
 export const filterFieldsProject = ["id_project", "createdAt", "updatedAt"];
 export const filterFieldsTeam = ["id_team", "createdAt", "updatedAt"];
